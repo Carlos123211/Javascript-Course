01e1ec1840936550c0580235449e1c6b2b8432b9
Carlos Gamarra <carlos.gamarra@goldenhippo.com>
2023-01-01 23:41:32 -0500

    About me exercise

--- activity/1_session/aboutme.js
+++ activity/1_session/aboutme.js
@@ -1,4 +1,7 @@
-console.log("hello world");
+const fullname = 'Carlos Enrique Gamarra Palacios'
+const career = 'Ingeniero de Sistemas'
+const jobTitle = 'Technical Product Owner'
 
+const sentence = `Hola, me llamo ${fullname} y me dedico a ${career}. Estoy cursando este Máster porque me gustaría trabar como ${jobTitle}`
 
-console.log("laater");
+console.log(sentence);
